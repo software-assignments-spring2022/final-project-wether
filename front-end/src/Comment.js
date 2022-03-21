d183e57e328acf3c73cd1242cb2bfce2b79e5d34
Brad Feng <bradfeng02@gmail.com>
2022-03-21 00:38:53 -0400

    added voting buttons to comment, improved styling

--- front-end/src/Comment.js
+++ front-end/src/Comment.js
@@ -5,10 +5,14 @@ import './Comment.css';
 
 const Comment = props => {
     return (
-        <div className={'comment-class'}>
-            <p>
+        <div className='comment-class'>
+            <p className='comment-content'>
                 {props.content}
             </p>
+            <div className='vote-buttons'>
+                <button className='rate rate-up'>👍</button>
+                <button className='rate rate-down'>👎</button>
+            </div>
         </div>
     )
 }
