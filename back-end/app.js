717978fcaf7807d3a04c46385aa694ac14e711b3
javajavascript <91429639+javajavascript@users.noreply.github.com>
2022-04-05 01:27:13 -0400

    Add files via upload
    added morgan, cors, and mongoose

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,14 @@
+require('dotenv').config({ silent: true })
 const express = require("express")
 const app = express()
+const morgan = require('morgan')
+const cors = require('cors') 
+const mongoose = require('mongoose')
+
+app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }))
+app.use(cors()) 
+
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })) 
 
 module.exports = app
