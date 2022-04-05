e6f292d732e3a634eb10226c099edbba351ce9d5
javajavascript <91429639+javajavascript@users.noreply.github.com>
2022-04-05 01:34:32 -0400

    Add files via upload
    added models for users and messages (comments)

---
+++ back-end/models/Message.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const messageSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    time {
+      type: String,
+      required: true,
+    },
+  }
+)
+
+const Message = mongoose.model('Message', messageSchema)
+module.exports = {
+  Message,
+}
