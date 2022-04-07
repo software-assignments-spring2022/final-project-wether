adf96442381b87176194f2b1310ebe28c49d44a5
javajavascript <91429639+javajavascript@users.noreply.github.com>
2022-04-07 04:07:58 -0400

    Add files via upload
    updated routes

--- back-end/server.js
+++ back-end/server.js
@@ -2,9 +2,9 @@
 
 const server = require("./app")
 
-require('dotenv').config({ silent: true })
+// require('dotenv').config({ silent: true })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 const listener = server.listen(port, function () {
   console.log(`Server running on port: ${port}`)
