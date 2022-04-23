0d796204ddc7439082c5e0e172f78e24bd9f8712
Hellen Ekpo <he560@nyu.edu>
2022-04-22 22:11:12 -0400

    this is the final update for the stakeholder meeting

--- front-end/src/Comments.js
+++ front-end/src/Comments.js
@@ -11,7 +11,8 @@ let username = ""
 const Comments = props => {
   const [cmtList, setCmtList] = useState([])
   const [content, setContent] = useState('')
-  //const [uid_1, setUID] = useState('')
+  const [user_name, setUser] = useState('')
+  const [pass_word, setPassword] = useState('')
 
   const postComment = (content, e) => {
     e.preventDefault()
@@ -29,6 +30,22 @@ const Comments = props => {
         console.log(err)
       })
     }
+  }
+  
+  const signout = (e) => {
+	  username = "";
+	  axios
+	  .post(`http://localhost:8080/signout`,{
+		  username: username,
+		  passowrd: ""
+	  })
+	  .then(
+	  	() => setUser(''), 
+		() => setPassword('')
+	  )
+	  .catch(err => {
+		  console.error(err)
+	  })
   }
   
   const get_username = () => {
@@ -98,6 +115,7 @@ const Comments = props => {
           <form>
             <div>
 			  {username === "" ? <Link to='/login'>Login</Link> : <p id= 'logged-in'>{username}</p>}
+			  {username === "" ? null : <button id='new-comment-submit' onClick={(e) => signout(e)}>Sign Out</button>}
               <button id='new-comment-submit' onClick={(e) => postComment(content, e)}>Post</button>
             </div>
             <input type='text' value={content} placeholder='add a comment' onChange={(e) => setContent(e.target.value)}/>
--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -2,10 +2,15 @@ import { Link } from 'react-router-dom'
 import sunny from './icons/sunny.png'
 import rain from './icons/rain.png'
 import cloud from './icons/cloud.png'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 //import search from './icons/search.png'
 import setting from './icons/setting.png'
 import story from './icons/story.png'
 import './Home.css'
+
+
+let username = ""
 
 const Home = props => {
   let tempSign = localStorage.getItem("tempSign");
@@ -13,6 +18,25 @@ const Home = props => {
     tempSign = 'F';
     localStorage.setItem("tempSign","F");
   }
+	
+  const get_username = () => {
+	  axios
+	  .get(`http://localhost:8080/username`)
+	  .then(response => {
+		  username = response.data.username
+	  })
+  }
+  useEffect(() => {
+    get_username()
+    const intervalHandle = setInterval(() => {
+      get_username()
+	  
+    }, 500)
+
+    return e => {
+      clearInterval(intervalHandle)
+    }
+  })
   return (
     <div className="Home">
       
